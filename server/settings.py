953aae0d99674bb5d13c899e88bfc8b7b0aff8b6
Minzhi Qu <quminzhi@gmail.com>
2022-02-16 15:56:32 -0800

    Added reset password

--- server/settings.py
+++ server/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-xi8%bqzrvp14kgn27-1=ftrsw9%ixb$lxhj=6zrh%i#g!hgv*7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
